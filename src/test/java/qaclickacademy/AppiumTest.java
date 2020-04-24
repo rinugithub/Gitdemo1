7a088dad1d4c40f97d471c697f4b8e7af7d82075
rinugithub <rinu.childofmothermary@gmail.com>
2020-04-24 18:27:44 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAndroid()
+	{
+		System.out.println("NativeAndroid");
+	}
+	@Test
+	public void IOSapps()
+	{
+		System.out.println("IOSapps");
+	}
+}
---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void posjira()
+	{
+		System.out.println("posjira");
+	}
+	@Test
+	public void deletetwitter()
+	{
+		System.out.println("deletetwitter");
+	}
+}
